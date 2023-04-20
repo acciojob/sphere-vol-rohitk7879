902c5fd9b529f70fd8525ab675ff5c0208af86d5
Rohit Kumawat <117036470+rohitk7879@users.noreply.github.com>
2023-04-20 23:56:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function volume_sphere() {
+function volume_sphere(event) {
+	// console.log(event);
+	event.preventDefault();
     //Write your code here
-  
+	const radius = document.getElementById("radius").value
+	let formula =(4/3)*3.14*(radius*radius*radius);
+	document.getElementById("volume").value = formula
+  // console.log(formula)
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
